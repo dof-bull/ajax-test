46ce9cf2403d746fdd741b1d0390496613085ea4
dof-bull <donalofarrell1@gmail.com>
2020-07-07 12:45:15 +0000

    Minor format to tables

--- main.js
+++ main.js
@@ -1,46 +1,48 @@
 const baseURL = "https://ci-swapi.herokuapp.com/api/";
 
 function getData(type, cb) {
-    var xhr = new XMLHttpRequest();
+  var xhr = new XMLHttpRequest();
 
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            cb(JSON.parse(this.responseText));
-        }
-    };
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      cb(JSON.parse(this.responseText));
+    }
+  };
 
-    xhr.open("GET", baseURL + type + "/");
-    xhr.send();
+  xhr.open("GET", baseURL + type + "/");
+  xhr.send();
 }
 
 function getTableHeaders(obj) {
-    var tableHeaders = [];
+  var tableHeaders = [];
 
-    Object.keys(obj).forEach(function (key) {
-        tableHeaders.push(`<td>${key}</td>`)
-    });
+  Object.keys(obj).forEach(function (key) {
+    tableHeaders.push(`<td>${key}</td>`);
+  });
 
-    return `<tr>${tableHeaders}</tr>`;
+  return `<tr>${tableHeaders}</tr>`;
 }
 
 function writeToDocument(type) {
-    var tableRows = [];
-    var el = document.getElementById("data");
-    el.innerHTML = "";
+  var tableRows = [];
+  var el = document.getElementById("data");
+  el.innerHTML = "";
 
-    getData(type, function (data) {
-        data = data.results;
-        var tableHeaders = getTableHeaders(data[0]);
+  getData(type, function (data) {
+    data = data.results;
+    var tableHeaders = getTableHeaders(data[0]);
 
-        data.forEach(function (item) {
-            var dataRow = [];
+    data.forEach(function (item) {
+      var dataRow = [];
 
-            Object.keys(item).forEach(function(key) {
-                dataRow.push(`<td>${item[key]}</td>`)
-            });
-            tableRows.push(dataRow);
-        });
-
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+      Object.keys(item).forEach(function (key) {
+        var rowData = item[key].toString();
+        var truncatedData = rowData.substring(0, 15);
+        dataRow.push(`<td>${truncatedData}</td>`);
+      });
+      tableRows.push(`<tr>${dataRow}</tr>`);
     });
+
+    el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+  });
 }
